43ae96c02a903a3a03eb1b398a5c63f3fdac9bb7
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-12 02:07:49 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>    
+      <Navbar/>     
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
 const Navbar = () => {
+
+  const [menu,setmenu] = useState("home")
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt='' className='logo'/>
       <ul className='navbar-menu'>
-        <li>home</li> 
-        <li>menu</li>  
-        <li>mobile-app</li>        
-        <li>contact us</li>        
+        <li onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</li> 
+        <li onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</li>  
+        <li onClick={()=>setmenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>        
+        <li onClick={()=>setmenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>        
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
