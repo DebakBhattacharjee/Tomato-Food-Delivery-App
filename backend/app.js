ef079f7d7ab2d149c15477d5c27f1d01aa95e644
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-15 16:15:20 +0530

    backend skeleton

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, this message is displayed on localhost!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
