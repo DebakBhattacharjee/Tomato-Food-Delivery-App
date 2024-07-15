ead0707295c8a9d60a30f571ab11cf74c3497713
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-15 17:21:48 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db.js'); 
+const router = express.Router();
 const port = 3000;
+// password: fooddelivery2024
+// username: Food-delivery-app-debak-tanmay
 
 
+
+
+// mongoose.connect('mongodb+srv://<username>:fooddelivery2024@cluster0.rb9shhw.mongodb.net/Food-delivery-app-debak-tanmay?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+//   console.log('Connected to MongoDB Atlas');
+// }).catch((err) => {
+//   console.error('Error connecting to MongoDB Atlas', err);
+// });
+
+
+// db connection
+connectDB();
 
 
 app.get('/', (req, res) => {
