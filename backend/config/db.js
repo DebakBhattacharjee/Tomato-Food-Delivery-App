35c6cd691e6aaabb7df77033370811e307a96a2e
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-15 17:55:43 +0530

    mongodb correction

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://Food-delivery-app-debak-tanmay:fooddelivery2024@cluster0.rb9shhw.mongodb.net/Food-delivery-app-debak-tanmay").then(console.log("DB connected"));
-}
-
-module.exports = connectDB;
