cab7765dae0e4736ad55b84fe551f705c428388d
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-19 17:54:36 +0530

    cart.jsx cart.css api changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Navbar.css'
-import {assets} from '../../assets/assets'
-import { Link } from 'react-router-dom'
-const Navbar = ({setShowLogin}) => {
+import { assets } from '../../assets/assets'
+import { Link, useNavigate } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
-  const [menu,setmenu] = useState("home")
+const Navbar = ({ setShowLogin }) => {
+
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount, token ,setToken } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate('/')
+  }
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt='' className='logo'/>
-      <ul className='navbar-menu'>
-        <Link to='/' onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</Link> 
-        <a href='#explore-menu' onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</a>  
-        <a href= '#app-download' onClick={()=>setmenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>        
-        <a href='footer' onClick={()=>setmenu("contact us")} className={menu==="contact us"?"active":""}>contact us</a>        
+      <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
+      <ul className="navbar-menu">
+        <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</a>
+        <a href='#app-download' onClick={() => setMenu("mob-app")} className={`${menu === "mob-app" ? "active" : ""}`}>mobile app</a>
+        <a href='#footer' onClick={() => setMenu("contact")} className={`${menu === "contact" ? "active" : ""}`}>contact us</a>
       </ul>
-      <div className='navbar-right'>
+      <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className='navbar-search-icon'>
+        <Link to='/cart' className='navbar-search-icon'>
           <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
-        </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+          <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
+        </Link>
+        {!token ? <button onClick={() => setShowLogin(true)}>sign in</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className='navbar-profile-dropdown'>
+              <li onClick={()=>navigate('/myorders')}> <img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
+              <hr />
+              <li onClick={logout}> <img src={assets.logout_icon} alt="" /> <p>Logout</p></li> 
+            </ul>
+          </div>
+        }
+
       </div>
-      
     </div>
   )
 }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,46 +1,95 @@
-import { createContext, useEffect, useState } from 'react';
-import { food_list} from '../assets/assets';
-
-
+import { createContext, useEffect, useState } from "react";
+import { food_list, menu_list } from "../assets/assets";
+import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
- 
-  const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000"
+    const [food_list, setFoodList] = useState([]);
+    const [cartItems, setCartItems] = useState({});
+    const [token, setToken] = useState("")
+    const currency = "₹";
+    const deliveryCharge = 50;
 
-  const addToCart =  (itemId) => {
-    if (!cartItems[itemId]) {
-        setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    const addToCart = async (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        }
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+        if (token) {
+            await axios.post(url + "/api/cart/add", { itemId }, { headers: { token } });
+        }
     }
-    else {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+
+    const removeFromCart = async (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+        if (token) {
+            await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } });
+        }
     }
-    
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            try {
+              if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }  
+            } catch (error) {
+                
+            }
+            
+        }
+        return totalAmount;
+    }
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data)
+    }
+
+    const loadCartData = async (token) => {
+        const response = await axios.post(url + "/api/cart/get", {}, { headers: token });
+        setCartItems(response.data.cartData);
+    }
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"))
+                await loadCartData({ token: localStorage.getItem("token") })
+            }
+        }
+        loadData()
+    }, [])
+
+    const contextValue = {
+        url,
+        food_list,
+        menu_list,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount,
+        token,
+        setToken,
+        loadCartData,
+        setCartItems,
+        currency,
+        deliveryCharge
+    };
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+
 }
-const removeFromCart =  (itemId) => {
-  setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
-}
 
-
-useEffect(()=>{
-  console.log(cartItems);
-  },[cartItems])
-
-  const contextValue ={
-      food_list,
-      setCartItems,
-      addToCart,
-      removeFromCart,
-      cartItems,
-
-  }
-
-  return (
-    <StoreContext.Provider value={contextValue}>
-      {props.children}
-    </StoreContext.Provider>
-  )
-}
-
-export default StoreContextProvider
+export default StoreContextProvider;
