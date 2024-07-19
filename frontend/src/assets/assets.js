b90db8d1d3882b17a5ae4f395fb0a285fcf4f196
Tanmay Dutta <103728522+Tanmay2484@users.noreply.github.com>
2024-07-19 16:36:34 +0530

    e

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import basket_icon from './basket_icon.png'
+//import basket_icon from './basket_icon.png'
 // import logo from './logo.png'
 // import header_img from './header_img.png'
 // import search_icon from './search_icon.png'
