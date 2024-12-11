112448953db37685435452223fdefb51b85fc5e3
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-12-11 16:37:58 +0530

    changing scroll position retention

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 
 import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -9,6 +11,17 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import ResForm from './components/ResForm/ResForm'
 import Checkout from './components/Checkout/Checkout'
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 const App = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,6 +30,7 @@ const App = () => {
 
   return (
     <>
+    <ScrollToTop />
     {showLogin && <LoginPopup setShowLogin={setShowLogin} setIsLoggedIn={setIsLoggedIn} />}
 
       <div className='app'>
